a8b8894ccb7317a311b6a10af7f141a69dfbbc09
David Garcia <david.garcia@deralia.com>
2024-08-16 18:25:34 +0200

    Drop the GitHub Pages settings
    Vercel seems to provide a better deployment experience for NextJS projects - because they are the NextJS creators

--- next.config.js
+++ next.config.js
@@ -1,24 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-
-    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-    // trailingSlash: true,
-
-    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-    // skipTrailingSlashRedirect: true,
-
-    // Optional: Change the output directory `out` -> `dist`
-    // distDir: 'dist',
-
-    // Set base path. This is the slug of your GitHub repository.
-    // @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-    basePath: "/nextjs-github-pages",
-
-    // Disable server-based image optimization. Next.js does not support
-    // dynamic features with static exports.
-    // @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-    images: { unoptimized: true },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
